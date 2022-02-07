62da7f2a6dcd54437edb985929675c7029234040
PawanJS <contactpawanjs@gmail.com>
2022-02-07 21:54:34 +0530

    Added Apply and Bind Method

--- script.js
+++ script.js
@@ -1,4 +1,14 @@
 'use strict';
+//* Call method
+//* call method is used to point this to another object
+//* with call we can borrow functions created in another object
+
+//* Making a independent function
+//* Passing parameters
+let printName = function (state, country = 'Not mentioned') {
+  console.log(`${this.name} ${state} ${country}`);
+};
+
 const userDetails = {
   name: 'Pawan',
   age: '24',
@@ -29,11 +39,28 @@ const userDetails3 = {
   },
 };
 
-userDetails.printDetails();
-userDetails3.calcDOB.call(userDetails);
+// userDetails.printDetails();
+// userDetails3.calcDOB.call(userDetails);
 
-userDetails.printDetails.call(userDetails2);
-userDetails3.calcDOB.call(userDetails2);
+// userDetails.printDetails.call(userDetails2);
+// userDetails3.calcDOB.call(userDetails2);
 
-userDetails.printDetails.call(userDetails3);
-userDetails3.calcDOB();
+// userDetails.printDetails.call(userDetails3);
+// userDetails3.calcDOB();
+
+// printName.call(userDetails, 'HP');
+// printName.call(userDetails2, 'Delhi');
+// printName.call(userDetails3, 'Chandigarh');
+
+//* Apply method
+//* There is slight difference between call and apply in call we pass arguments one by one to avoid this mess we can pass all arguments as an array in apply method at once
+
+// printName.apply(userDetails, ['Himachal', 'India']);
+// printName.apply(userDetails3, ['Delhi', 'India']);
+// printName.apply(userDetails3, ['Chandigarh', 'India']);
+
+//* Bind Method
+//* Bind works similarly like call but rather then calling function directly it creates a copy of the original function and invoke it whenever we need it
+
+let newName = printName.bind(userDetails, ['Himachal', 'India']);
+newName();
